6c3e248a4ec90b684972919f9400bf67b28e5651
Projeto Resiliências Climáticas <162454027+rclimaticas@users.noreply.github.com>
2024-12-11 16:53:34 +0000

    :bug: fix: middlewares

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,62 +1,34 @@
-// import { NextFunction, Request, Response } from "express";
-// import jwt from "jsonwebtoken";
-// import dotenv from "dotenv";
-
-
-// type TokenPayload = {
-//     id: string;
-//     iat: number;
-//     exp: number;
-// };
-
-// export function AuthMiddleware(
-//     req: Request,
-//     res: Response,
-//     next: NextFunction
-// ) {
-//     const { authorization } = req.headers; 
-
-//     if (!authorization) {
-//         return res.status(401).json({ error: "token nao fornecido" });
-//     }
-
-//     const [, token]= authorization.split(" ");
-
-//     try {
-//         const decoded = jwt.verify(token, "my_secret");
-//         const { id } = decoded as TokenPayload;
-
-//         req.userId = id;
-//         next();
-//     } catch (error) {
-//         return res.status(401).json({ error: "token invalido" });
-//     }
-// }
-
 import { NextFunction, Request, Response } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-declare module "express-serve-static-core" {
-    interface Request {
-        userId?: string;
+
+type TokenPayload = {
+    id: string;
+    iat: number;
+    exp: number;
+};
+
+export function AuthMiddleware(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const { authorization } = req.headers; 
+
+    if (!authorization) {
+        return res.status(401).json({ error: "token nao fornecido" });
     }
-}
 
-export function AuthMiddleware(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization?.split(" ")[1];
-
-    if (!token) {
-        return res.status(401).json({ error: "Token não fornecido." });
-    }
+    const [, token]= authorization.split(" ");
 
     try {
-        const secret = process.env.SECRET_KEY || "default_secret";
-        const decoded = jwt.verify(token, secret) as JwtPayload;
+        const decoded = jwt.verify(token, process.env.SECRET_KEY || "default_secret");
+        const { id } = decoded as TokenPayload;
 
-        req.userId = decoded.id as string;
-        next(); 
+        req.userId = id;
+        next();
     } catch (error) {
-        console.error("Erro ao verificar token");
-        return res.status(401).json({ error: "Token inválido ou expirado." });
+        return res.status(401).json({ error: "token invalido" });
     }
 }
