cff2bae2b93a983a9ea21a39737acedb57c09c17
Projeto Resiliências Climáticas <162454027+rclimaticas@users.noreply.github.com>
2024-12-11 21:00:28 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaa

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
+import * as jwt from "jsonwebtoken";
 
 type TokenPayload = {
     id: string;
